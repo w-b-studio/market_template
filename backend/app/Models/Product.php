c91c2dd3f1568a44946c6a1bda7da69eefa59788
Insar <58511185+3961683@users.noreply.github.com>
2022-05-19 01:40:17 +0600

    Add files via upload

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name','price','category_id', 'request_id'
+    ];
+
+    function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    function images() {
+        return $this->hasMany(Image::class);
+    }
+
+    function request() {
+        return $this->belongsTo(Request::class);
+    }
+}
